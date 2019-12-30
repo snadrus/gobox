a347f534bb87b4d917d09df3dbceaccf54545183
Andy Jackson <snadrus@gmail.com>
2019-12-29 19:58:08 -0800

    comment

--- gobox.go
+++ gobox.go
@@ -9,6 +9,7 @@ type ObjFunc func(c *gin.Context) interface{}
 //
 func StartServer(func(apiRoutes gin.RouterGroup)) {
 	// todo further cripple the API to HTML(template, objfunc), JSON(objfunc), group
+	// what abuot filestores? S3 bucket *?
 }
 
 // TODO component registration & use? or just leave it?
